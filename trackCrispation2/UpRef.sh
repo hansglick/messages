370f493aaa50ed66c65d431e0bb250f7e873bd34
HansGlick <hansglick@gmail.com>
2020-02-05 10:38:03 +0100

    first

---
+++ trackCrispation2/UpRef.sh
@@ -0,0 +1,8 @@
+cd /home/osboxes/proj/messages/trackCrispation2
+
+/home/osboxes/anaconda3/bin/jupyter nbconvert --to script /home/osboxes/proj/messages/trackCrispation2/BuildBatch.ipynb
+/home/osboxes/anaconda3/envs/twitter/bin/python /home/osboxes/proj/messages/trackCrispation2/BuildBatch.py
+
+
+/home/osboxes/anaconda3/bin/jupyter nbconvert --to script /home/osboxes/proj/messages/trackCrispation2/BuildRef.ipynb
+/home/osboxes/anaconda3/envs/twitter/bin/python /home/osboxes/proj/messages/trackCrispation2/BuildRef.py
